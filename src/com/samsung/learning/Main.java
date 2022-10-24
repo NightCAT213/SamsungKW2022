c58f84bbea87b850327019f2ebd68908464692a6
NightCAT213 <73906284+NightCAT213@users.noreply.github.com>
2022-10-24 15:04:59 +0300

    Update Main.java

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -1,42 +1,30 @@
 package com.samsung.learning;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // в начале была задача
+        int arr2[][] = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] array = new int[n];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = sc.nextInt();
+        System.out.println(arr2[1][2]);
+
+
+        int[][] arr = new int[5][4];
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr[j].length; i++) {
+                arr[j][i] = (int) (Math.random() * 100);
+            }
         }
-
-
-        for (int i: array) { // для всех элементов из массива
-            System.out.println(i);
+        for (int j = 0;  j < arr.length; j++) {
+            for (int i = 0; i < arr[j].length; i++) {
+                System.out.print(arr[j][i] + "\t");
+            }
+            System.out.println("\n");
         }
-
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(sum(a, b));
-    }
-
-    public static int sum(int a, int b) {
-        return a + b;
-    }
-
-    public static void printArray(int[] array) {
-        System.out.println("[");
-        for (int i = 0; i < array.length - 1; i++) {
-            System.out.println(array[i] + ", ");
+        for (int i = 0; i < arr[i].length; i++) {
+            System.out.print(arr[i][i] + " ");
         }
-        System.out.println(array[array.length - 1] + "]");
-    }
-
-    public static int max(int a, int b) {
-        return a > b ? a : b;
     }
 }
