03526b9b1e934dbf7dc2a05047ca458610180598
NightCAT213 <73906284+NightCAT213@users.noreply.github.com>
2022-11-08 20:02:38 +0300

    Update Main.java

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -1,27 +1,19 @@
 package com.samsung.learning;
-
 import com.samsung.learning.Person;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person();
-        person.name = "Valera";
-        person.age = 18;
-
-        Person person1 = new Person();
-        person1.name = "Kolya";
-        person1.age = 14;
-        // if private
-        // Person person = new Person("Sasha", 17);
+        Person person = new Person("Ivan", 18);
+        Person person1 = person;
+        Person person2 = new Person(person);
 
         person.printInfo();
-        person.birthday();
-        person.printInfo();
 
-        person1.printInfo();
+        person1.setAge(19);
+        person2.setAge(19);
 
-        // Integer
-        Integer a = 1;
-        String s = a.toString();
+        System.out.println(person.getAge());
+        System.out.println(person1.getAge());
+        System.out.println(person2.getAge());
     }
 }
