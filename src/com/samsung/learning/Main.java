4a44cda082b32c97ce079cbea500f15087c88df0
NightCAT213 <73906284+NightCAT213@users.noreply.github.com>
2022-11-15 09:33:03 +0300

    Update Main.java

--- src/com/samsung/learning/Main.java
+++ src/com/samsung/learning/Main.java
@@ -1,1 +1,17 @@
-// None, but only now
+package com.samsung.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(product.count);
+
+        Product сыр = new Product("сыр", 350, 10);
+        Product хлеб = new Product("хлеб", 50, 100);
+
+        Product.changeCount(сыр, -5);
+
+        // Integer.getInteger()
+        System.out.println(сыр);
+
+        // Math.команда()
+    }
+}
